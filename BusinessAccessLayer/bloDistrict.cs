ccc17bd134b944a8b3b6a81a5e07e2180afa40cd
Nibin Benny <nibinbenny47@gmail.com>
2021-02-15 13:06:23 +0530

    BusinessAccesslayer containing district and place CRUD operation

---
+++ BusinessAccessLayer/bloDistrict.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BusinessAccessLayer
+{
+    public class bloDistrict
+    {
+        public dloDistrict obj = new dloDistrict();
+
+        public void inserData(string name)
+        {
+            obj.insertData(name);
+
+        }
+        //delete district name by id
+        public void deleteData(int id)
+        {
+            obj.deleteData(id);
+        }
+        //Fill repeater with district names
+        public DataTable fillRepeater()
+        {
+            return obj.fillRepeater();
+        }
+        /* fill repeater with district names by id */
+        public DataTable selectDistrict(int id)
+        {
+            return obj.selectDistrict(id);
+        }
+        /* update district names */
+        public void updateData(int id, string name)
+        {
+            obj.updateData(id, name);
+        }
+    }
+}
+
---
+++ BusinessAccessLayer/bloPlace.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using DataAccessLayer;
+
+namespace BusinessAccessLayer
+{
+    public class bloPlace
+    {
+        public dloPlace obj = new dloPlace();
+        /* dropdown filling */
+        public DataTable fillDistrict()
+        {
+            return obj.fillDistrict();
+        }
+        /* insert place  names*/
+        public void insertPlace(string placename, int id)
+        {
+            obj.insertPlace(placename, id);
+        }
+        //Fill repeater with place names
+        public DataTable fillRepeater()
+        {
+            return obj.fillRepeater();
+        }
+        //Delete place names 
+        public void deleteData(int id)
+        {
+            obj.deleteData(id);
+        }
+        public DataTable selectPlace(int id)
+        {
+            return obj.selectPlace(id);
+        }
+        /* update place names */
+        public void updateData(int id, string name, int did)
+        {
+            obj.updateData(id, name, did);
+        }
+    }
+}
